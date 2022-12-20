635e85d2100583679098679b65d2a15295eaa07f
yakir <yakir442@gmail.com>
2022-12-20 15:10:30 +0200

    try

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,7 +3,7 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
+    println!("Guss the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
